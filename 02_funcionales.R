c0548fd8320c8ea4566b490b68ba88bb29aa1273
Santiago Ortúzar <saortuza@uc.cl>
2024-07-08 22:00:20 -0400

    corregir ruta indicada

--- 02_funcionales.R
+++ 02_funcionales.R
@@ -33,7 +33,7 @@ carga_librerias(librerias = librerias)
 # Fijar directorios ------------------------------------------------------------
 ### Ubicación del proyecto #####################################################
 folder_project <- rprojroot::find_rstudio_root_file()
-folder_here <- paste0(folder_project,"/02_funcionales")
+folder_here <- paste0(folder_project)
 
 ### Bases de datos #############################################################
 source(paste0(folder_project,"/aux_dirs_input.R"))
