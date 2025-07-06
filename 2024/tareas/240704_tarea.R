d45c39a624ee48d8071742b8c7857b128008e049
Santiago Ortúzar <sa.ortuzar@gmail.com>
2025-07-06 19:48:02 -0400

    mover todo a carpeta 2024 + corregir ruta de importación de bbdd

--- 2024/tareas/240704_tarea.R
+++ 2024/tareas/240704_tarea.R
@@ -1,20 +1,20 @@
-
 # Limpiar environment ----------------------------------------------------------
-rm(list=ls())
+rm(list = ls())
 
 # Cargar librerías -------------------------------------------------------------
 ### Crear vector de librerías ##################################################
-librerias <- c("tidyverse",
-               "ggplot2", # crear gráficos
-               "feather", # cargar BBDD CASEN
-               "gapminder" # contiene BBDD que usaremos
-               )
+librerias <- c(
+  "tidyverse",
+  "ggplot2", # crear gráficos
+  "feather", # cargar BBDD CASEN
+  "gapminder" # contiene BBDD que usaremos
+)
 
 ### Definir función para cargar librerías ######################################
 carga_librerias <- function(librerias) {
   for (i in librerias) {
-    esta_disponible <- require(i, character.only = TRUE, quietly= TRUE)
-    if(esta_disponible){
+    esta_disponible <- require(i, character.only = TRUE, quietly = TRUE)
+    if (esta_disponible) {
       library(i, character.only = TRUE)
     } else {
       install.packages(i)
@@ -29,10 +29,10 @@ carga_librerias(librerias = librerias)
 # Definir directorios ----------------------------------------------------------
 ### Definir ubicación del proyecto #############################################
 folder_project <- rprojroot::find_rstudio_root_file()
-folder_here <- folder_project
+folder_here <- paste0(folder_project, "/2024")
 
 ### Definir ubicación del script que contiene las rutas de las BBDD ############
-source(paste0(folder_here,"/aux_dirs_input.R"))
+source(paste0(folder_here, "/auxiliar/aux_dirs_input.R"))
 
 # Cargar BBDD CASEN ------------------------------------------------------------
 casen <- feather::read_feather(file_casen)
@@ -46,23 +46,24 @@ casen <- feather::read_feather(file_casen)
 
 ### Definir función sum_something() ############################################
 sum_something <- function(data, group_var, var) {
-  data %>% 
-    group_by(!!enexpr(group_var)) %>% 
+  data %>%
+    group_by(!!enexpr(group_var)) %>%
     summarise(n = sum(!!enexpr(var)))
 }
 
 ### Probar función sum_something() #############################################
 sum_something(
   ###### Seleccionar BBDD ######
-  data=casen,
+  data = casen,
   ###### Seleccionar variable de agrupación ######
-  group_var=region,
+  group_var = region,
   ###### Seleccionar variable que vamos a sumar ######
-  var=ytotcor)
+  var = ytotcor
+)
 
 ### Definir función plot_table() ###############################################
-plot_table <- function(table, x_var, y_var,  input_title ) {
-  ggplot(table, aes(x = !!enexpr(x_var), y = !!enexpr(y_var) )) +
+plot_table <- function(table, x_var, y_var, input_title) {
+  ggplot(table, aes(x = !!enexpr(x_var), y = !!enexpr(y_var))) +
     geom_bar(stat = "identity") +
     labs(title = input_title)
 }
@@ -72,7 +73,7 @@ plot_table <- function(table, x_var, y_var,  input_title ) {
 tabla <- sum_something(casen, region, ytotcor)
 
 ###### Ejecutar plot_table() sobre el output creado con sum_something() ######
-plot_table(tabla, region, n,  "Total del ingreso por región" )
+plot_table(tabla, region, n, "Total del ingreso por región")
 
 ###### Eliminar objeto usado en ejemplo ######
 rm(tabla)
@@ -85,13 +86,15 @@ rm(tabla)
 gapminder_list <- split(gapminder, gapminder$year)
 
 ### Usar purrr para generar un gráfico para cada año ###########################
-plots_by_year <- gapminder_list %>% 
-  map(sum_something, continent, pop) %>% 
-  map(plot_table, continent, n, "Población mundial, según continente" )
+plots_by_year <- gapminder_list %>%
+  map(sum_something, continent, pop) %>%
+  map(plot_table, continent, n, "Población mundial, según continente")
 
 ### Limpiar environment ########################################################
-rm(gapminder_list,
-   plots_by_year)
+rm(
+  gapminder_list,
+  plots_by_year
+)
 
 # EJERCICIO 1 ------------------------------------------------------------------
 ### Diapositivas ###############################################################
@@ -101,54 +104,53 @@ rm(gapminder_list,
 gapminder_list <- split(gapminder, gapminder$year)
 
 ### Veamos bien qué hace el for loops que nos entregan #########################
-plot_with_for <- 
+plot_with_for <-
   function(
-    ### El argumento tablas es una LISTA de DATA FRAMES
-    tablas)
-    {
+      ### El argumento tablas es una LISTA de DATA FRAMES
+      tablas) {
     ### Abre el environment de la función
-    
+
     ### En plots almacena una lista compuesta de un vector de longitud igual a la longitud del argumento tablas
-    plots <- list(vector(length = length(tablas) ))
+    plots <- list(vector(length = length(tablas)))
     ### Por lo tanto, si gapminder_list tiene una longitud de 12, la lista en plots tendrá una longitud de 12 elementos/vectores
-    
+
     ### Se define i como 1
     i <- 1
-    
+
     ### Abre un loop que itera sobre cada data frame en el argumento tablas
     for (plot in tablas) {
       ### Crea el objeto table usando la función sum_something()
       table <- sum_something(
         ### data=
-        plot, 
+        plot,
         ### group_var=
-        continent, 
+        continent,
         ### var=
         pop
         ### Cierra sum_something()
-        )
-      
+      )
+
       ### Guarda en el lugar i dentro de plots el siguiente objeto plot_table
       plots[[i]] <- plot_table(
-        
+
         ### table=
-        table, 
+        table,
         ### x_var=
-        continent, 
+        continent,
         ### y_var=
-        n, 
+        n,
         ### input_title=
-        paste("Población mundial, según continente. Año", plot$year[1] )
+        paste("Población mundial, según continente. Año", plot$year[1])
         ### Cierra plot_table()
-        )
-      
+      )
+
       ### Se redefine i como el i actual + 1
-      i <-  i + 1
+      i <- i + 1
     }
-    
+
     ### Devuelve el objeto plots
     return(plots)
-}
+  }
 
 plots <- plot_with_for(gapminder_list)
 
@@ -156,33 +158,47 @@ plots <- plot_with_for(gapminder_list)
 ### Crea una función llamada plot_with_purrr que reciba una lista de tablas y devuelva una lista de gráficos
 
 ### Limpiar environment ########################################################
-rm(plot_with_for,
-   plots)
+rm(
+  plot_with_for,
+  plots
+)
 
 ### Crear un plot individualmente ##############################################
 gapminder_list[["1952"]] %>%
   ### Crear tabla
-  sum_something(group_var=continent,
-                var=pop) %>%
+  sum_something(
+    group_var = continent,
+    var = pop
+  ) %>%
   ### Crear plot
-  plot_table(x_var=continent,
-             y_var=n,
-             input_title=paste("Población mundial, según continente. Año", 
-                               as.character(unique(gapminder_list[["1952"]]$year)),
-                               "\n(PLOT HECHO INDIVIDUALMENTE)"))
+  plot_table(
+    x_var = continent,
+    y_var = n,
+    input_title = paste(
+      "Población mundial, según continente. Año",
+      as.character(unique(gapminder_list[["1952"]]$year)),
+      "\n(PLOT HECHO INDIVIDUALMENTE)"
+    )
+  )
 
 ### Ahora hagamos la serie completa con map() ##################################
 ej1_prueba <- purrr::map(
-  .x=gapminder_list,
-  ~sum_something(data=.x,
-                 group_var=continent,
-                 var=pop) %>% 
-    plot_table(x_var=continent,
-               y_var=n,
-               input_title=paste("Población mundial, según continente. Año",
-                                 as.character(unique(.x$year)),
-                                 "\n(PLOT HECHO CON MAP, SIN EMPAQUETAR EN UNA FUNCION)"))
-  )
+  .x = gapminder_list,
+  ~ sum_something(
+    data = .x,
+    group_var = continent,
+    var = pop
+  ) %>%
+    plot_table(
+      x_var = continent,
+      y_var = n,
+      input_title = paste(
+        "Población mundial, según continente. Año",
+        as.character(unique(.x$year)),
+        "\n(PLOT HECHO CON MAP, SIN EMPAQUETAR EN UNA FUNCION)"
+      )
+    )
+)
 
 ### Ver los resultados #########################################################
 ### Esto nos da una lista de gráficos con el año en el título
@@ -192,28 +208,34 @@ ej1_prueba[["1957"]]
 ### Empaquetar código en una función ###########################################
 plot_with_purrr <- function(
     ### El argumento tablas toma como input una lista de data frames
-  tablas)
-{
+    tablas) {
   ### Abrir environment de la función
-  
+
   ### Aplicar map()
   plots <- purrr::map(
-    .x=tablas,
-    ~sum_something(data=.x,
-                   group_var=continent,
-                   var=pop) %>% 
-      plot_table(x_var=continent,
-                 y_var=n,
-                 input_title=paste("Población mundial, según continente. Año",
-                                   as.character(unique(.x$year)),
-                                   "\n(PLOT HECHO CON MAP,\nEMPAQUETADO EN UNA FUNCION)")))
-  
+    .x = tablas,
+    ~ sum_something(
+      data = .x,
+      group_var = continent,
+      var = pop
+    ) %>%
+      plot_table(
+        x_var = continent,
+        y_var = n,
+        input_title = paste(
+          "Población mundial, según continente. Año",
+          as.character(unique(.x$year)),
+          "\n(PLOT HECHO CON MAP,\nEMPAQUETADO EN UNA FUNCION)"
+        )
+      )
+  )
+
   ### Devolver plots
   return(plots)
 }
 
 ### Probar función #############################################################
-ej1_resultado <- plot_with_purrr(tablas=gapminder_list)
+ej1_resultado <- plot_with_purrr(tablas = gapminder_list)
 
 ### Ver los resultados #########################################################
 ej1_resultado[["1952"]]
@@ -225,43 +247,55 @@ ej1_resultado[["1957"]]
 
 ### Modificar función plot_table() #############################################
 plot_table_mod <- function(
-    table, 
-    x_var, 
-    y_var, 
+    table,
+    x_var,
+    y_var,
     input_title,
     subtitulo) {
-  ggplot(table, 
-         aes(x = !!enexpr(x_var), 
-             y = !!enexpr(y_var))) +
+  ggplot(
+    table,
+    aes(
+      x = !!enexpr(x_var),
+      y = !!enexpr(y_var)
+    )
+  ) +
     geom_bar(stat = "identity") +
-    labs(title = input_title,
-         subtitle = subtitulo)
+    labs(
+      title = input_title,
+      subtitle = subtitulo
+    )
 }
 
 ### Crear versión modificada de plot_with_purrr() ##############################
 plot_with_purrr_mod <- function(
     ### El argumento tablas toma como input una lista de data frames
-  tablas)
-  {
+    tablas) {
   ### Abrir environment de la función
-  
+
   ### Aplicar map()
   plots <- purrr::map(
-    .x=tablas,
-    ~sum_something(data=.x,
-                   group_var=continent,
-                   var=pop) %>% 
-      plot_table_mod(x_var=continent,
-                     y_var=n,
-                     input_title="Población mundial, según continente",
-                     subtitulo=paste0(as.character(unique(.x$year)),
-                                      " (AHORA EL AÑO VA EN EL SUBTITULO)")))
+    .x = tablas,
+    ~ sum_something(
+      data = .x,
+      group_var = continent,
+      var = pop
+    ) %>%
+      plot_table_mod(
+        x_var = continent,
+        y_var = n,
+        input_title = "Población mundial, según continente",
+        subtitulo = paste0(
+          as.character(unique(.x$year)),
+          " (AHORA EL AÑO VA EN EL SUBTITULO)"
+        )
+      )
+  )
   ### Devolver plots
   return(plots)
 }
 
 ### Aplicar función modificada #################################################
-ej2_resultado <- plot_with_purrr_mod(tablas=gapminder_list)
+ej2_resultado <- plot_with_purrr_mod(tablas = gapminder_list)
 
 ### Ver los resultados #########################################################
 ej2_resultado[["1952"]]
@@ -274,7 +308,7 @@ ej2_resultado[["1957"]]
 ### Revisar función asignada ###################################################
 nested_for <- function(v1, v2) {
   for (x in v1) {
-    for (y in v2){
+    for (y in v2) {
       print(paste(x, y))
     }
   }
@@ -289,65 +323,68 @@ nested_for(1:3, 5:8)
 ### ...elementos de Y
 
 ### El ejercicio consiste en escribir una función llamada nested_map()...
-### ...que utilice una sintaxis de purrr. 
+### ...que utilice una sintaxis de purrr.
 
 ### Definir función nestep_map() ###############################################
 nested_map <- function(
     ### Argumento 1
     vectorX,
     ### Argumento 2
-    vectorY
-    ){
-  ### Abrir ambiente de la función 
-  
+    vectorY) {
+  ### Abrir ambiente de la función
+
   ### Función de segundo orden
   valorX <- map(
     ### Argumento 1
-    .x=vectorX,
+    .x = vectorX,
     ### Función
-    function(x){
+    function(x) {
       ### Habilitar otro environment de función
-      
+
       ### Habilitar comentarios
-      cat(paste0("\nPara el valor ",as.character(unique(x)),"...\n"))
-      
+      cat(paste0("\nPara el valor ", as.character(unique(x)), "...\n"))
+
       ### Función de primer orden
       valorY <- map(
         ### Argumento 2
-        .x=vectorY,
+        .x = vectorY,
         ### Función
-        function(y){
+        function(y) {
           ### Habilitar otro environment de función más
-          
+
           ### Habilitar comentarios
-          cat(paste0("\n...asigna el valor ",as.character(unique(y)),"\n"))
-          
+          cat(paste0("\n...asigna el valor ", as.character(unique(y)), "\n"))
+
           ### Crear combinación de X e Y
-          combinacion_x_y <- paste(x,y)
-          
+          combinacion_x_y <- paste(x, y)
+
           ### Imprime combinación de X e Y
           print(combinacion_x_y)
-          
+
           ### Cerrar environment de función de primer orden
-          }
+        }
       )
-      
+
       ### Cerrar environment de función de segundo orden
     }
   )
-  
+
   ### Cerrar environment de función nested_map()
-  }
+}
 
 ### Probar función nested_map() ################################################
 nested_map(1:3, 5:8)
 
 ### Probar con vectores de longitud muy diferente ##############################
-nested_map(1:3,1:30)
+nested_map(1:3, 1:30)
 
 ### Probar con vectores de distinta naturaleza #################################
-nested_map(c("a","b","c","d"),
-           1:10)
+nested_map(
+  c("a", "b", "c", "d"),
+  1:10
+)
 
-nested_map(rep("ola",10),
-           rep("ola",10))
+nested_map(
+  rep("ola", 10),
+  rep("ola", 10)
+)
